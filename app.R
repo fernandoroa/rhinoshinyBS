54267091632f4c0b25ed63462aa1f10462657f56
Fernando Roa O <fernando@appsilon.com>
2022-10-08 19:09:39 -0300

    fix: add dependency to renv

--- app.R
+++ app.R
@@ -1,4 +1,11 @@
-# Rhino / shinyApp entrypoint. Do not edit.
+#
+#   shinyBS not detected by renv (in fact) when added in dependencies only
+#   this is for renv to work only
+#
+library(shinyBS)
+
+# does not work
 requireNamespace("shinyBS")
 shinyBS:::.onAttach()
+
 rhino::app()
--- dependencies.R
+++ dependencies.R
@@ -1,4 +1,4 @@
 # This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
 library(rhino)
 # does not work:
-# library(shinyBS)
+library(shinyBS)
